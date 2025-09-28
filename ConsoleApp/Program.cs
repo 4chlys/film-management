052fa3c9fea56d1bc08fddc5d37ac55004c36ab5
romeo.weyns <romeo.weyns@student.kdg.be>
2025-09-28 20:50:11 +0200

    Program.cs has been completed for implementation and test of Service classes.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using ConsoleApp.Models;
+using ConsoleApp.Services;
+
+namespace ConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var dataService = new DataService();
+        var menuService = new MenuService(dataService);
+
+        // Seed the data
+        dataService.Seed();
+
+        Console.WriteLine("Welcome to Film Management System!");
+            
+        bool running = true;
+        while (running)
+        {
+            menuService.ShowMainMenu();
+                
+            if (int.TryParse(Console.ReadLine(), out int choice))
+            {
+                if (choice == 0)
+                {
+                    running = false;
+                    Console.WriteLine("Goodbye!");
+                }
+                else
+                {
+                    menuService.HandleMenuChoice(choice);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
+        }
+    }
+}
