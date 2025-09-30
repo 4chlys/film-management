82007b3e3a7ad5ac96ced4f6929ceccf341efb26
romeo.weyns <romeo.weyns@student.kdg.be>
2025-09-30 14:08:36 +0200

    Actor.cs - simplified and correction in relationship with Film

--- ConsoleApp/Models/Actor.cs
+++ ConsoleApp/Models/Actor.cs
@@ -9,35 +9,22 @@ public class Actor
     public DateTime DateOfBirth { get; set; }
     public int? Age { get; set; }
 
-    public Actor(string name, DateTime dateOfBirth)
-    {
-        Name = name ?? throw new ArgumentNullException(nameof(name));
-        DateOfBirth = dateOfBirth;
-    }
-    
     public IReadOnlyList<Film> Films => _films.AsReadOnly();
     
     public void AddFilm(Film film) => _films.Add(film);
-    public void RemoveFilm(Film film) => _films.Remove(film);
-    public void ClearFilms() => _films.Clear();
-    
-    public void AddFilmsRange(IEnumerable<Film> films) => _films.AddRange(films);
 
     public override string ToString()
     {
         int displayAge = Age ?? CalculateAge();
-        var filmCount = Films.Count;
-        return $"{Name} from {Nationality}, born {DateOfBirth:yyyy-MMM-dd} (age {displayAge}) - {filmCount} film(s)";
+        return $"{Name} from {Nationality}, born {DateOfBirth:yyyy-MMM-dd} (age {displayAge})";
     }
 
     private int CalculateAge()
     {
         var today = DateTime.Today;
         int age = today.Year - DateOfBirth.Year;
-        
         if (DateOfBirth.Date > today.AddYears(-age))
             age--;
-            
         return age;
     }
 }
