a27316a8e092775cbf13d0307e42db2366230a3d
romeo.weyns <romeo.weyns@student.kdg.be>
2025-09-30 14:09:06 +0200

    FilmDirector.cs - simplified and correction in relationship with Film

--- ConsoleApp/Models/FilmDirector.cs
+++ ConsoleApp/Models/FilmDirector.cs
@@ -4,28 +4,17 @@ public class FilmDirector
 {
     private readonly List<Film> _films = new();
 
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public string Country { get; set; } = string.Empty;
-    public DateTime CareerStart { get; set; }
+    public int YearStarted { get; set; }
 
-    public FilmDirector(string name, DateTime careerStart)
-    {
-        Name = name;
-        CareerStart = careerStart;
-    }
-    
     public IReadOnlyList<Film> Films => _films.AsReadOnly();
     
     public void AddFilm(Film film) => _films.Add(film);
-    public void RemoveFilm(Film film) => _films.Remove(film);
-    public void ClearFilms() => _films.Clear();
-    
-    public void AddFilmsRange(IEnumerable<Film> films) => _films.AddRange(films);
 
     public override string ToString()
     {
-        var filmCount = Films.Count;
-        var yearsActive = DateTime.Now.Year - CareerStart.Year;
-        return $"{Name} from {Country}, directing since {CareerStart.Year} ({yearsActive} years) - {filmCount} film(s)";
+        var yearsActive = DateTime.Now.Year - YearStarted;
+        return $"{Name} from {Country}, directing since {YearStarted} ({yearsActive} years)";
     }
 }
