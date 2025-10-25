a5a5bae4b8a2c528a8d2d89a5c95cd1288eab9e9
romeo.weyns <romeo.weyns@student.kdg.be>
2025-10-25 21:14:09 +0200

    EF implemented.

---
+++ DAL/EF/FilmDbContext.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using FilmManagement.BL.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace FilmManagement.DAL.EF;
+
+public class FilmDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Film> Films { get; set; }
+    public DbSet<Actor> Actors { get; set; }
+    public DbSet<FilmDirector> Directors { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlite("Data source=filmmanagement.db");
+        }
+
+        optionsBuilder.LogTo(message => Debug.WriteLine(message),
+            LogLevel.Information);
+    }
+
+    public bool CreateDatabase(bool dropDatabase)
+    {
+        if (dropDatabase)
+        {
+            Database.EnsureDeleted();
+        }
+        return Database.EnsureCreated();
+    }
+}
