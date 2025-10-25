650726b24db3f6b11e57099142d5a1ff3a092f7e
romeo.weyns <romeo.weyns@student.kdg.be>
2025-10-25 21:15:32 +0200

    Extension class adjusted.

--- UI-CA/Extensions/ActorExtensions.cs
+++ UI-CA/Extensions/ActorExtensions.cs
@@ -6,16 +6,6 @@ public static class ActorExtensions
 {
     public static string GetInfo(this Actor actor)
     {
-        int displayAge = actor.Age ?? CalculateAge(actor);
-        return $"{actor.Name}, {actor.Nationality}, born {actor.DateOfBirth:yyyy-MMM-dd} (age {displayAge})";
-    }
-
-    private static int CalculateAge(Actor actor)
-    {
-        var today = DateTime.Today;
-        int age = today.Year - actor.DateOfBirth.Year;
-        if (actor.DateOfBirth.Date > today.AddYears(-age))
-            age--;
-        return age;
+        return $"{actor.Name}, {actor.Nationality}, born {actor.DateOfBirth:yyyy-MMM-dd} (age {actor.Age})";
     }
 }
